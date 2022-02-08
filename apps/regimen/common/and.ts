d542af68a74cbd181935eef68c967b571b6268f9
evanrs <me@evanrs.com>
2022-02-07 20:53:46 -0500

    feat: add 'and gate' for collections

---
+++ apps/regimen/common/and.ts
@@ -0,0 +1,27 @@
+/**
+ * andV — an "and gate" for a collection Value
+ *
+ * Will return value if found as a Value of source
+ */
+export function andV<T extends string>(source: unknown, value?: T) {
+  if (value == null) return;
+
+  return Array.isArray(source)
+    ? source.includes(value)
+      ? value
+      : undefined
+    : Object.values(source as Record<string, T>).includes(value)
+    ? value
+    : undefined;
+}
+
+/**
+ * andK — an "and gate" for a collection Key
+ *
+ * Will check for presence as element of an array
+ */
+export function andK<T extends string>(source: unknown, value?: T) {
+  if (value == null) return;
+
+  return value in (source as Record<T, unknown>) ? value : undefined;
+}
