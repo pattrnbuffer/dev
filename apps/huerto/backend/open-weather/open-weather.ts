ec42adc315f81507d6ab9e54501b01be5ec02b66
evanrs <me@evanrs.com>
2022-02-21 16:40:15 -0500

    docs: note rate limits

--- apps/huerto/backend/open-weather/open-weather.ts
+++ apps/huerto/backend/open-weather/open-weather.ts
@@ -1,4 +1,13 @@
 /**
+ * Open Weaaaaather
+ *
+ * Limit:
+ *  1,000,000 calls per month | 60 calls per minute
+ *  = 23 calls per minute = 23.15 = 1,000,000 / (30 * 24 * 60)
+ *  = 6480 spare retries  =  0.15 * (30 * 24 * 60)
+ *  = 2.63 second update interval
+ *
+ * https://openweathermap.org/price
  * https://openweathermap.org/api/one-call-api
  * https://openweathermap.org/darksky-openweather
  */
