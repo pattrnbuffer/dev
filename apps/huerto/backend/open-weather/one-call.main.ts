7b8f24dcc2f77a6fbb298b33d6178e810aea6e09
evanrs <me@evanrs.com>
2022-03-03 23:26:58 -0500

    fix(huerto/open-weather): onel-call url

--- apps/huerto/backend/open-weather/one-call.main.ts
+++ apps/huerto/backend/open-weather/one-call.main.ts
@@ -1,12 +1,28 @@
 #! /usr/bin/env yarn ts-node
 
+import { argv } from 'zx';
 import { location } from './config';
 import { oneCall } from './one-call';
 
 require.main === module && main();
 async function main() {
-  const weather = await oneCall({ location });
+  const [type] = argv._ as ['url' | 'weather'];
 
-  console.log(weather);
-  console.table(weather.current);
+  switch (type) {
+    case 'url':
+      const url = oneCall.url({ location });
+
+      console.log(url);
+
+      return;
+
+    case 'weather':
+    default:
+      const weather = await oneCall({ location });
+
+      console.log(weather);
+      console.table(weather.current);
+
+      return;
+  }
 }
