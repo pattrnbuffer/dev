8a8e794e036f6f9cdc0bb666fec87974acddf4e1
evanrs <me@evanrs.com>
2022-04-07 22:05:45 -0400

    refactor(hooks): type useTrigger

--- packages/hooks/use-trigger.ts
+++ packages/hooks/use-trigger.ts
@@ -1,5 +1,13 @@
-export function useTrigger(effect, deps = []) {
-  const [fire, setFire] = useState(0)
-  useEffect(effect, [fire, ...deps])
-  return useCallback(() => setFire((pulls) => pulls + 1), [])  
+import {
+  useEffect,
+  useCallback,
+  useState,
+  EffectCallback,
+  DependencyList,
+} from 'react';
+
+export function useTrigger(effect: EffectCallback, deps: DependencyList = []) {
+  const [fire, setFire] = useState(0);
+  useEffect(effect, [fire, ...deps]);
+  return useCallback(() => setFire(pulls => pulls + 1), []);
 }
