b716858566609b7cf22e715fe70b3868cf3ae884
evanrs <me@evanrs.com>
2022-05-11 11:28:42 -0400

    refactor(hooks): add deps to useMountedRef to control mount lifecycle

--- packages/hooks/use-mounted-ref.ts
+++ packages/hooks/use-mounted-ref.ts
@@ -1,32 +1,40 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef, RefObject, DependencyList } from 'react';
 
 export type MountedRef = ReturnType<typeof useMountedRef>;
 
-export function useMountedRef() {
+/**
+ * A toolbelt for mount phases
+ * - deps: undefined => mounted with component
+ * - deps: null => mounted per render
+ * - deps: [...] => mounted per effect
+ */
+export function useMountedRef(deps?: undefined | null | DependencyList) {
   const state = useRef(true);
 
   useEffect(
     () => () => {
       state.current = false;
     },
-    [],
+    // we assume our mount state tracks the component
+    // to track each render we pass null as a signal
+    // otherwise we track effect state
+    deps === undefined ? [] : deps === null ? undefined : deps,
   );
 
-  return useMemo(() => {
-    const mounted = () => state.current;
+  return useMemo(() => createMountedRef(state), []);
+}
 
-    mounted.commit = <R>(call: () => R) => {
-      if (state.current) return call();
-    };
+export function createMountedRef(state: RefObject<Boolean>) {
+  const mounted = () => state.current;
 
-    mounted.guard =
-      <Args extends any[], Fn extends (...args: Args) => any>(callback: Fn) =>
-      (...params: Args) =>
-        mounted.commit(() => callback(...params));
+  const commit = <R>(call: () => R) => {
+    if (mounted()) return call();
+  };
 
-    /** @deprecated */
-    mounted.callback = mounted.guard;
+  const guard =
+    <Args extends any[], Fn extends (...args: Args) => any>(callback: Fn) =>
+    (...params: Args) =>
+      commit(() => callback(...params));
 
-    return mounted;
-  }, []);
+  return Object.assign(mounted, { commit, guard });
 }
