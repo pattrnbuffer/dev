bb4c1ce48e1072dc05676a3c584b5b1a79b72f11
evanrs <me@evanrs.com>
2022-05-11 17:48:46 -0400

    feat(hooks-web): add hooks-web ... we're not calling them web-hooks

---
+++ packages/hooks-web/use-audio-context.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useMemo } from 'react';
+
+const AudioContext = window?.AudioContext;
+const AudioContextContext = createContext<AudioContext>(
+  new AudioContext({ latencyHint: 'interactive', sampleRate: 48000 }),
+);
+
+export const AudioContextProvider = AudioContextContext.Provider;
+
+/**
+ * move to  @dev/whooks … web
+ * consider @dev/uhooks … universal
+ * or even  @dev/nhooks … native
+ */
+export function useAudioContext(
+  customContext?: AudioContextOptions | InstanceType<typeof AudioContext>,
+) {
+  const parentContext = useContext(AudioContextContext);
+
+  const hasContext = customContext instanceof AudioContext;
+  const hasProps =
+    !hasContext &&
+    customContext != null &&
+    ('frequency' in customContext || 'sampleRate' in customContext);
+
+  return useMemo<AudioContext>(
+    () =>
+      hasContext
+        ? customContext
+        : hasProps
+        ? new AudioContext(customContext)
+        : parentContext,
+    // dependency list
+    hasContext
+      ? [customContext]
+      : hasProps
+      ? Object.values(customContext)
+      : [parentContext],
+  );
+}
