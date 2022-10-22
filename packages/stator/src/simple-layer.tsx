907372cda97ccd56f026925f62e103ea7e6e845c
evanrs <me@evanrs.com>
2022-10-21 22:06:28 -0400

    feat(stator): add simple layer

---
+++ packages/stator/src/simple-layer.tsx
@@ -0,0 +1,61 @@
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+const SimpleLayerContext = createContext<number>(0);
+
+type SLFC<T> = FC<{
+  children: ReactNode;
+  transform: (reduced: T | undefined, update: T) => T;
+}>;
+
+export const Provider: SLFC<number> = ({ children, transform }) => {
+  const context = useContext(SimpleLayerContext);
+  const [value, setValue] = useState<number>(() =>
+    transform(undefined, context),
+  );
+  useEffect(() => setValue(state => transform(state, context)), [context]);
+
+  return (
+    <SimpleLayerContext.Provider value={value}>
+      {children}
+    </SimpleLayerContext.Provider>
+  );
+};
+
+export function useSimpleLayer() {
+  // returns the context value for the layer
+  // (transform already applied at the context level)
+  //
+  // in jotai we would read from the getter of the atom
+  //
+  // to have an atom for a layer we need the provider to create one
+  // while also reading from the layer above
+  //
+  // it must initialize the atom with the proper state
+  // - it will set the atom to a transformation of the parent value
+
+  // it must update the atom at the right time with the proper state
+  // - use effect will run when the parent context value changes
+  //   and set the atom to a transformation of the new parent value
+
+  // it must return the value and efficiently memoize
+  // - it requires no memoization, simple layer is just an incrementing number
+
+  // could it not have overlap of layers?
+  // - that could only be possible if each provider
+  //   (which establishes the same layer for each depth of the tree)
+  //   could produce the proper super set of both
+  //   as a simple layer key there would be no signficane
+  //   you are a member of your current and every prior and have a
+  //   value equal to your depth
+
+  const context = useContext(SimpleLayerContext);
+
+  return context.value;
+}
