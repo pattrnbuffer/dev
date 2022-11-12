71568b751e6eb49d642b730123541d08de1fc392
evanrs <me@evanrs.com>
2022-11-12 16:49:07 -0500

    refactor(tools): export createCSSUnit

--- packages/tools/src/cssunit.ts
+++ packages/tools/src/cssunit.ts
@@ -1,13 +1,18 @@
 type CSSInput = REM | PX;
 type REM = `${number}rem` | `${number} rem`;
 type PX = `${number}px` | `${number} px`;
+type CSSUnitType = 'rem' | 'px';
 
-export function CSSUnit<T extends CSSInput>(input: T, base: number = 16) {
+export function CSSUnit<T extends CSSInput>(input: T, base = 16) {
   const [value, type] = [
     Number(input.replace(/([^.0-9])/g, '')),
-    input.replace(/[^pxrem]/g, ''),
+    input.toLowerCase().replace(/[^pxrem]/g, ''),
   ];
 
+  return createCSSUnit(value, type as CSSUnitType, base);
+}
+
+export function createCSSUnit(value: number, type: CSSUnitType, base = 16) {
   const [rem, px] =
     type === 'rem' ? [value, value * base] : [value / base, value];
 
