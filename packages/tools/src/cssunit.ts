00a4c4fa788bfa28bbe3138cae5dd274e92705bb
evanrs <me@evanrs.com>
2023-01-08 23:48:05 -0500

    chore(tools): pull out regex

--- packages/tools/src/cssunit.ts
+++ packages/tools/src/cssunit.ts
@@ -3,10 +3,13 @@ type REM = `${number}rem` | `${number} rem`;
 type PX = `${number}px` | `${number} px`;
 type CSSUnitType = 'rem' | 'px';
 
+const digitsRx = /([^.0-9])/g;
+const unitRx = /[^pxrem]/g;
+
 export function CSSUnit<T extends CSSInput>(input: T, base = 16) {
   const [value, type] = [
-    Number(input.replace(/([^.0-9])/g, '')),
-    input.toLowerCase().replace(/[^pxrem]/g, ''),
+    Number(input.replace(digitsRx, '')),
+    input.toLowerCase().replace(unitRx, ''),
   ];
 
   return createCSSUnit(value, type as CSSUnitType, base);
